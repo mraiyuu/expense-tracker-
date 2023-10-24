60446aaeab3e6467ace723d4bd9a79e8e85a55f4
mraiyuuu <munenedenis618@gmail.com>
2023-10-24 14:54:16 +0300

    final

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
     return (
-        <li className="minus">
-            {transaction.text} <span>{sign}${transaction.amount}</span><button className="delete-btn">x</button>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
         </li>
     )
 }
